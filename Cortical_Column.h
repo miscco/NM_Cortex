31734861ffbcc958b4b35dfc7c3539b94187cfa8
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-09-13 10:54:13 +0200

    Cleaned up the files and added some helpers

--- Cortical_Column.h
+++ Cortical_Column.h
@@ -26,171 +26,146 @@
  *				PLoS Computational Biology. 2014;10:e1003923
  */
 
-/************************************************************************************************/
-/*								Header file of a cortical module								*/
-/************************************************************************************************/
+/******************************************************************************/
+/*						Implementation of a cortical module					  */
+/******************************************************************************/
 #pragma once
 #include <cmath>
 #include <vector>
 #include "Random_Stream.h"
-using std::vector;
 
-/****************************************************************************************************/
-/*										 		end			 										*/
-/****************************************************************************************************/
-
-
-/****************************************************************************************************/
-/*									Macro for vector initialization									*/
-/****************************************************************************************************/
-#ifndef _INIT
-#define _INIT(x)	{x, 0.0, 0.0, 0.0, 0.0}
-#endif
-/****************************************************************************************************/
-/*										 		end			 										*/
-/****************************************************************************************************/
-
-
-/****************************************************************************************************/
-/*								Implementation of the cortical module 								*/
-/****************************************************************************************************/
 class Cortical_Column {
 public:
-	/* Constructors */
-	Cortical_Column(void)
-	{set_RNG();}
+    Cortical_Column(double* Par)
+        :sigma_p 	(Par[0]),	  g_KNa		(Par[1]),	dphi		(Par[2])
+    {set_RNG();}
 
-	Cortical_Column(double* Par)
-	 :sigma_p 	(Par[0]),	  g_KNa		(Par[1]),	dphi		(Par[2])
-	{set_RNG();}
-
-	/* Initialize the RNGs */
-	void 	set_RNG		(void);
-
-	/* Set strength of input */
-	void	set_input	(double I) {input = I;}
-
-	/* Firing rates */
-	double 	get_Qp		(int) const;
-	double 	get_Qi		(int) const;
-
-	/* Currents */
-	double 	I_ep		(int) const;
-	double 	I_ei		(int) const;
-	double 	I_gp		(int) const;
-	double 	I_gi		(int) const;
-	double 	I_L_p		(int) const;
-	double 	I_L_i		(int) const;
-	double 	I_KNa		(int) const;
-
-	/* Potassium pump */
-	double 	Na_pump		(int) const;
-
-	/* Noise function */
-	double 	noise_xRK 	(int, int) const;
-	double 	noise_aRK 	(int) const;
-
-	/* ODE functions */
-	void 	set_RK		(int);
-	void 	add_RK	 	(void);
-	void	iterate_ODE	(void);
-
-	/* Data storage  access */
-	friend void get_data (int, Cortical_Column&, vector<double*>);
-
-	/* Stimulation protocol access */
-	friend class Stim;
-
+    void	set_input	(double I) {input = I;}
+    void	iterate_ODE	(void);
 private:
-	/* Random number generators */
-	vector<random_stream_normal>        MTRands;
+    /* Initialize the RNGs */
+    void 	set_RNG		(void);
 
-	/* Container for noise */
-	vector<double>	Rand_vars;
+    /* Firing rates */
+    double 	get_Qp		(int) const;
+    double 	get_Qi		(int) const;
 
-	/* Declaration and Initialization of parameters */
-	/* Membrane time in ms */
-	const double 	tau_p 		= 30;
-	const double 	tau_i 		= 30;
+    /* Currents */
+    double 	I_ep		(int) const;
+    double 	I_ei		(int) const;
+    double 	I_gp		(int) const;
+    double 	I_gi		(int) const;
+    double 	I_L_p		(int) const;
+    double 	I_L_i		(int) const;
+    double 	I_KNa		(int) const;
 
-	/* Maximum firing rate in ms^-1 */
-	const double 	Qp_max		= 30.E-3;
-	const double 	Qi_max		= 60.E-3;
+    /* Potassium pump */
+    double 	Na_pump		(int) const;
 
-	/* Sigmoid threshold in mV */
-	const double 	theta_p		= -58.5;
-	const double 	theta_i		= -58.5;
+    /* Noise function */
+    double 	noise_xRK 	(int, int) const;
+    double 	noise_aRK 	(int) const;
 
-	/* Sigmoid gain in mV */
-	const double 	sigma_p		= 4;
-	const double 	sigma_i		= 6;
+    /* ODE functions */
+    void 	set_RK		(int);
+    void 	add_RK	 	(void);
 
-	/* Scaling parameter for sigmoidal mapping (dimensionless) */
-	const double 	C1		= (3.14159265/sqrt(3));
+    /* Helper functions */
+    inline std::vector<double> init (double value)
+    {return {value, 0.0, 0.0, 0.0, 0.0};}
+    inline void add_RK (std::vector<double>& var)
+    {var[0] = (-3*var[0] + 2*var[1] + 4*var[2] + 2*var[3] + var[4])/6;}
 
-	/* Parameters of the firing adaption */
-	const double 	alpha_Na	= 2.;			/* Sodium influx per spike  in mM ms 	*/
-	const double 	tau_Na		= 1.;			/* Sodium time constant	    in ms 	*/
+    /* Random number generators */
+    std::vector<random_stream_normal>        MTRands;
 
-	const double 	R_pump   	= 0.09;        	/* Na-K pump constant	    in mM/ms 	*/
-	const double 	Na_eq    	= 9.5;         	/* Na-eq concentration	    in mM 	*/
+    /* Container for noise */
+    std::vector<double>	Rand_vars;
 
-	/* PSP rise time in ms^-1 */
-	const double 	gamma_e		= 70E-3;
-	const double 	gamma_g		= 58.6E-3;
+    /* Declaration and Initialization of parameters */
+    /* Membrane time in ms */
+    const double 	tau_p 		= 30;
+    const double 	tau_i 		= 30;
 
-	/* Conductivities */
-	/* Leak  in aU*/
-	const double 	g_L    		= 1.;
+    /* Maximum firing rate in ms^-1 */
+    const double 	Qp_max		= 30.E-3;
+    const double 	Qi_max		= 60.E-3;
 
-	/* Synaptic conductivity in ms */
-	const double 	g_AMPA 		= 1.;
-	const double 	g_GABA 		= 1.;
+    /* Sigmoid threshold in mV */
+    const double 	theta_p		= -58.5;
+    const double 	theta_i		= -58.5;
 
-	/* KNa in mS/cm^-2 */
-	const double	g_KNa		= 1.33;
+    /* Sigmoid gain in mV */
+    const double 	sigma_p		= 4;
+    const double 	sigma_i		= 6;
 
-	/* Reversal potentials in mV */
-	/* Synaptic */
-	const double 	E_AMPA  	= 0;
-	const double 	E_GABA  	= -70;
+    /* Scaling parameter for sigmoidal mapping (dimensionless) */
+    const double 	C1		= (3.14159265/sqrt(3));
 
-	/* Leak */
-	const double 	E_L_p 		= -66;
-	const double 	E_L_i 		= -64;
+    /* Parameters of the firing adaption */
+    const double 	alpha_Na	= 2.;			/* Sodium influx per spike  in mM ms 	*/
+    const double 	tau_Na		= 1.;			/* Sodium time constant	    in ms 	*/
 
-	/* Potassium */
-	const double 	E_K    		= -100;
+    const double 	R_pump   	= 0.09;        	/* Na-K pump constant	    in mM/ms 	*/
+    const double 	Na_eq    	= 9.5;         	/* Na-eq concentration	    in mM 	*/
 
-	/* Noise parameters in ms^-1 */
-	const double 	mphi		= 0.0;
-	const double	dphi		= 20E-1;
-	double			input		= 0.0;
+    /* PSP rise time in ms^-1 */
+    const double 	gamma_e		= 70E-3;
+    const double 	gamma_g		= 58.6E-3;
 
-	/* Connectivities (dimensionless) */
-	const double 	N_pp		= 120;
-	const double 	N_ip		= 72;
-	const double 	N_pi		= 90;
-	const double 	N_ii		= 90;
+    /* Conductivities */
+    /* Leak  in aU*/
+    const double 	g_L    		= 1.;
 
-	/* Interation parameters for SRK4 */
-	const vector<double> A = {0.5, 0.5, 1.0, 1.0};
-	const vector<double> B = {0.75, 0.75, 0.0, 0.0};
+    /* Synaptic conductivity in ms */
+    const double 	g_AMPA 		= 1.;
+    const double 	g_GABA 		= 1.;
 
-	/* Population variables */
-	vector<double> 	Vp		= _INIT(E_L_p),		/* excitatory membrane voltage						*/
-					Vi		= _INIT(E_L_i),		/* inhibitory membrane voltage						*/
-					Na		= _INIT(Na_eq),		/* Na concentration									*/
-					s_ep	= _INIT(0.0),		/* PostSP from excitatory to excitatory population	*/
-					s_ei	= _INIT(0.0),		/* PostSP from excitatory to inhibitory population	*/
-					s_gp	= _INIT(0.0),		/* PostSP from inhibitory to excitatory population	*/
-					s_gi	= _INIT(0.0),		/* PostSP from inhibitory to inhibitory population	*/
-					x_ep	= _INIT(0.0),		/* derivative of s_ep								*/
-					x_ei	= _INIT(0.0),		/* derivative of s_ei								*/
-					x_gp	= _INIT(0.0),		/* derivative of s_gp				 				*/
-					x_gi	= _INIT(0.0);		/* derivative of s_gi	 							*/
+    /* KNa in mS/cm^-2 */
+    const double	g_KNa		= 1.33;
+
+    /* Reversal potentials in mV */
+    /* Synaptic */
+    const double 	E_AMPA  	= 0;
+    const double 	E_GABA  	= -70;
+
+    /* Leak */
+    const double 	E_L_p 		= -66;
+    const double 	E_L_i 		= -64;
+
+    /* Potassium */
+    const double 	E_K    		= -100;
+
+    /* Noise parameters in ms^-1 */
+    const double 	mphi		= 0.0;
+    const double	dphi		= 20E-1;
+    double			input		= 0.0;
+
+    /* Connectivities (dimensionless) */
+    const double 	N_pp		= 120;
+    const double 	N_ip		= 72;
+    const double 	N_pi		= 90;
+    const double 	N_ii		= 90;
+
+    /* Interation parameters for SRK4 */
+    const std::vector<double> A = {0.5, 0.5, 1.0, 1.0};
+    const std::vector<double> B = {0.75, 0.75, 0.0, 0.0};
+
+    /* Population variables */
+    std::vector<double> Vp	= init(E_L_p), /* excitatory membrane voltage						*/
+                        Vi	= init(E_L_i),	/* inhibitory membrane voltage						*/
+                        Na	= init(Na_eq),	/* Na concentration									*/
+                        s_ep= init(0.0),	/* PostSP from excitatory to excitatory population	*/
+                        s_ei= init(0.0),	/* PostSP from excitatory to inhibitory population	*/
+                        s_gp= init(0.0),	/* PostSP from inhibitory to excitatory population	*/
+                        s_gi= init(0.0),	/* PostSP from inhibitory to inhibitory population	*/
+                        x_ep= init(0.0),	/* derivative of s_ep								*/
+                        x_ei= init(0.0),	/* derivative of s_ei								*/
+                        x_gp= init(0.0),	/* derivative of s_gp				 				*/
+                        x_gi= init(0.0);	/* derivative of s_gi	 							*/
+    /* Data storage  access */
+    friend void get_data (int, Cortical_Column&, std::vector<double*>);
+
+    /* Stimulation protocol access */
+    friend class Stim;
 };
-/****************************************************************************************************/
-/*										 		end			 										*/
-/****************************************************************************************************/
-
